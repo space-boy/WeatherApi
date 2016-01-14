b351e5fc20791cdc4bde3ae033c1485d8f80640e
coldhands92 <cjones9022@gmail.com>
2016-01-14 19:52:14 +0000

    prepare TDD for new preferenceFragment

---
+++ app/src/androidTest/java/com/sunrise/weather/weatherapi/PreferenceFragmentTest.java
@@ -0,0 +1,7 @@
+package com.sunrise.weather.weatherapi;
+
+/* before creating a new fragment for preferences, which will eventually be wrapped in
+* a dialog on phones, and a fragment included on the detail screen for bigger devices
+* */
+public class PreferenceFragmentTest {
+}
